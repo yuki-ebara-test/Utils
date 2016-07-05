af40529c9f5bbe229c4f18d776814656b0cc1e0a
YukiEbara <ebara1979@hotmail.co.jp>
2016-07-05 22:42:53 +0900

    Mavenの導入＆JMockitを導入

--- test/test/utils/poi/ExcelMngTest.java
+++ test/test/utils/poi/ExcelMngTest.java
@@ -2,9 +2,14 @@ package test.utils.poi;
 
 import static org.junit.Assert.*;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.junit.Test;
 
+import mockit.Mock;
+import mockit.MockUp;
 import utils.poi.ExcelManager;
+import utils.poi.ExcelRow;
+import utils.poi.ExcelSheet;
 
 public class ExcelMngTest {
 
@@ -32,4 +37,29 @@ public class ExcelMngTest {
 		}
 	}
 
+	@Test
+    public void testMock() {
+		// 指定クラスの関数の内容を改変
+		new MockUp<ExcelSheet>() {
+            @Mock
+            public void addExcelRow (ExcelRow row) {}
+        };
+		new MockUp<ExcelRow>() {
+            @Mock
+            public String convertToString(Cell cell) {
+            	return "";
+            }
+        };
+
+		try {
+			ExcelManager target = ExcelManager.createExcelManager("D:\\pleiades\\workspace\\Utils\\tmp\\POItest.xlsx");
+
+			System.out.println(target);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+
+    }
 }
